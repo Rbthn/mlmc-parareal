346cf4a08b6ddd83e70db0b484cc005d6f5038c7
Robert Hahn <robert.hahn@tu-darmstadt.de>
2024-07-05 11:27:52 +0200

    added script for heat eqn MLMC

---
+++ scripts/heat_fem_MLMC.jl
@@ -0,0 +1,33 @@
+using DrWatson
+@quickactivate "MLMC_Parareal"
+
+include(srcdir("experiment.jl"))
+include(srcdir("problem.jl"))
+include(srcdir("models/heat_fem.jl"))
+
+n = 11
+x = range(0.0, 1.0, n)
+u_0 = [0.0 for _ in range(1, n - 2)]
+u_left = 0.5
+u_right = 1.0
+
+cv = [1.0 for _ in range(1, n - 1)]
+k = [1.0 for _ in range(1, n - 1)]
+Q = [0.0 for _ in range(1, n - 1)]
+
+t_0 = 0.0
+t_end = 1.0
+Δt_0 = 0.1
+
+use_parareal = false
+p = Heat_Problem(u_0, u_left, u_right, t_0, t_end, Δt_0, x, cv, k, Q)
+
+deviation = 0.5
+L = 4
+ϵ = 1e-3
+qoi_fn = L2_squared
+
+e = MLMC_Experiment(p, qoi_fn, Uniform(-deviation, deviation), L, ϵ; use_parareal)
+result = run(e)
+
+print("done")
