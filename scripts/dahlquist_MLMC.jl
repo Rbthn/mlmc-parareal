4fe291050dc63fe98c57671a4d87d1c57898ea8e
Robert Hahn <robert.hahn@tu-darmstadt.de>
2024-07-18 18:19:11 +0200

    cleaning up scripts

--- scripts/dahlquist_MLMC.jl
+++ scripts/dahlquist_MLMC.jl
@@ -1,19 +1,30 @@
 using DrWatson
 @quickactivate :MLMC_Parareal
 
-
+# problem
 u_0 = 1.0
 t_0 = 0.0
-t_end = 1.0
+t_end = 8.0
 λ = -1.0
 Δt_0 = 0.1
-use_parareal = false
 p = Dahlquist_Problem(u_0, t_0, t_end, λ, Δt_0)
 
+# parareal
+parareal_args = Parareal_Args(num_intervals=8, tolerance=1e-4)
+
+# MLMC
 deviation = 0.5
 L = 4
 ϵ = 1e-3
 qoi_fn = L2_squared
 
-e = MLMC_Experiment(p, qoi_fn, Uniform(-deviation, deviation), L, ϵ; use_parareal)
+e = MLMC_Experiment(p, qoi_fn, Uniform(-deviation, deviation),
+    L, ϵ, use_parareal=true, parareal_args=parareal_args)
 result = run(e)
+
+#save with git commit hash (and patch if repo is dirty)
+problem_name = "Dahlquist"
+name = savename(problem_name, result["settings"], "jld2")
+tagsave(datadir("simulations", name), result, storepatch=true)
+
+print("done")
--- scripts/heat_fem_MLMC.jl
+++ scripts/heat_fem_MLMC.jl
@@ -1,6 +1,7 @@
 using DrWatson
 @quickactivate :MLMC_Parareal
 
+# problem
 n = 11
 x = range(0.0, 1.0, n)
 u_0 = [0.0 for _ in range(1, n - 2)]
@@ -15,15 +16,24 @@ t_0 = 0.0
 t_end = 1.0
 Δt_0 = 0.1
 
-use_parareal = false
 p = Heat_Problem(u_0, u_left, u_right, t_0, t_end, Δt_0, x, cv, k, Q)
 
+# parareal
+parareal_args = Parareal_Args(num_intervals=4, tolerance=1e-4)
+
+# MLMC
 deviation = 0.5
 L = 4
 ϵ = 1e-3
 qoi_fn = L2_squared
 
-e = MLMC_Experiment(p, qoi_fn, Uniform(-deviation, deviation), L, ϵ; use_parareal)
+e = MLMC_Experiment(p, qoi_fn, Uniform(-deviation, deviation),
+    L, ϵ, use_parareal=true, parareal_args=parareal_args)
 result = run(e)
+
+#save with git commit hash (and patch if repo is dirty)
+problem_name = "HeatFEM"
+name = savename(problem_name, result["settings"], "jld2")
+tagsave(datadir("simulations", name), result, storepatch=true)
 
 print("done")
