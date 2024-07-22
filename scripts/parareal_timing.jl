8f0649e49d3e56c31b148f34871b495acffa765f
Robert Hahn <robert.hahn@tu-darmstadt.de>
2024-07-22 16:50:07 +0200

    added measured runtimes to timing plot

--- scripts/parareal_timing.jl
+++ scripts/parareal_timing.jl
@@ -17,40 +17,61 @@ N = 10
 L = 15
 benchmark_time = 30
 
-### Plot number of timesteps over iteration number k
-plt = plot(xlabel=L"iteration $k$",
-    xticks=1:N,
-    ylabel="number of timesteps",
-    yaxis=:log,
-    ylim=(1e4, 3e6),
-    minorgrid=:on,
-    legend=:bottomright
-    #title="total and sequential number of timesteps over Parareal iterations"
-)
-colors = palette(:viridis, 3)
+sol_1 = solve(p, [L, L], 0, use_parareal=false)
+bench_1 = @benchmark solve($p, [$L, $L], 0, use_parareal=false) seconds = benchmark_time
 
-println("Reference solution:")
-sol_1 = @btime solve($p, [$L, $L], 0, use_parareal=false) seconds = benchmark_time
 ts_parareal = zeros(2, N)
+actual_parareal = zeros(1, N)
 for k in range(1, N)
 
     # use zero tolerance to force iteration until i=k
     parareal_args = Parareal_Args(
         num_intervals=10, tolerance=0, max_iterations=k)
-    println("====================================")
-    println("Parareal, k=$k:")
-    sol_2 = @btime solve($p, [$L, $L], 0, use_parareal=true, parareal_args=$parareal_args) seconds = benchmark_time
+    sol_2 = solve(p, [L, L], 0, use_parareal=true, parareal_args=parareal_args)
+    bench_2 = @benchmark solve($p, [$L, $L], 0, use_parareal=true, parareal_args=$parareal_args) seconds = benchmark_time
 
     ### compare number of timesteps (total, sequential)
 
     # seq. and total timesteps are equal for purely sequential solution
     ts_parareal[:, k] .= sol_2.stats.timesteps
+    actual_parareal[k] = minimum(bench_2.times)
 end
 
-hline!(plt, [sol_1.stats.naccept], label="reference solution", linestyle=:dash, color=:black)
-scatter!(plt, ts_parareal[1, :], label="total", color=colors[1])
-scatter!(plt, ts_parareal[2, :], label="sequential", color=colors[2])
+plt = plot(xlabel=L"iteration $k$",
+    xticks=1:N,
+    ylabel="number of timesteps",
+    ylim=(0, 2e6),
+    minorgrid=:on,
+    legend=:topleft
+    #title="total and sequential number of timesteps over Parareal iterations"
+)
+colors = palette(:viridis, 3)
 
+# plot timesteps
+reference_ts = sol_1.stats.naccept
+hline!(plt, [reference_ts], label="reference solution", linestyle=:dash, color=:black)
+scatter!(plt, ts_parareal[1, :], label="total timesteps", color=colors[1])
+scatter!(plt, ts_parareal[2, :], label="sequential timesteps", color=colors[2])
+
+# scale second yaxis
+ax_actual = twinx(plt)
+reference_bench = minimum(bench_1.times)
+scale = 1e-6 # ns to ms
+limits = ylims(plt)
+ratio = (reference_ts - limits[1]) / (limits[2] - limits[1])
+#actual_max = 350 # maximum value in ms
+#actual_min = 1 / (ratio - 1) * (ratio * actual_max - scale * reference_bench)
+actual_min = 0
+actual_max = 1 / ratio * (scale * reference_bench + (ratio - 1) * actual_min)
+ylims!(ax_actual, actual_min, actual_max)
+
+# plot actual runtimes
+ylabel!(ax_actual, "runtime [ms]")
+hline!(ax_actual, [scale * reference_bench], label=nothing, linestyle=:dash, color=:black)
+scatter!(ax_actual, scale * actual_parareal[:], label=nothing, color=colors[3])
+scatter!(plt, fill(NaN, 1:N), label="measured runtime", color=colors[3])
+
+# save plot
 wsave(plotsdir(savename(
         "parareal_timing",
         (problem=p.name, level=L, K=N),
