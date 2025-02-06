0706695bc5d747df076e4c0f5f945442d2079488
Robert Hahn <robert.hahn@tu-darmstadt.de>
2025-02-06 10:24:02 +0100

    added benchmark section to im_3kW script

--- scripts/im_3kW_extracted.jl
+++ scripts/im_3kW_extracted.jl
@@ -89,3 +89,80 @@ parareal_args = (;
     fine_args=(; dt=dt),
     shared_memory=false,
 )
+
+
+
+# %% Benchmark
+
+nruns = 10      # number of runs over which to average
+ncores = 100    # numer of parallel evaluations assumed for benchmark
+
+# fine cost ref, fine cost para, total cost ref, total cost para
+timing = zeros(nruns, 4)
+
+for i = 1:nruns
+    # determine cost of single eval per level
+    costs = fill(Inf, L + 1)
+    for l = 0:L
+        costs[l+1] = @belapsed begin
+            n_params = length($dists)
+            params = transform.($dists, rand(n_params)) #.* [dK, dM]
+            prob = instantiate_problem($p, params)
+            sol = DifferentialEquations.solve(
+                prob, $p.alg;
+                dt=compute_timestep($p, $l)
+            )
+            qoi = qoi_fn(sol)
+        end seconds = 10
+    end
+
+    # determine cost on finest level (with parareal)
+    cost_para = @belapsed begin
+        n_params = length($dists)
+        params = transform.($dists, rand(n_params)) #.* [dK, dM]
+        prob = instantiate_problem($p, params)
+        sol, _ = Parareal.solve(
+            prob, $p.alg;
+            dt=compute_timestep($p, $L),
+            parareal_args...
+        )
+        qoi = qoi_fn(sol)
+    end seconds = 10
+
+    # run MultilevelEstimators once to determine number of samples per level
+    e_ref = MLMC_Experiment(p, qoi_fn, dists,
+        L, 1e-2;
+        use_parareal=false,
+        cost_model=(l -> costs[l[1]+1])
+    )
+    res_ref = run(
+        e_ref;
+        continuate=false,
+        do_mse_splitting=true,
+        min_splitting=0.01,
+        warmup_samples=3
+    )
+
+    nb_of_samples = res_ref["history"][:nb_of_samples]
+
+    # MultilevelEstimators computes samples from levels sequentially.
+    # As such, total time is the sum of time each level takes (plus convergence estimate overhead)
+    # given the number of cores, the time it takes to compute the samples for a
+    # given level equals the number of sequential runs due to insufficient cores
+    # needed on that level times the average runtime on that level
+    level_times = fill(Inf, L + 1)
+
+    # integer divide, round up
+    div_up = (x, y) -> ceil(Int, x / y)
+
+    seq_runs = div_up.(nb_of_samples, ncores)
+
+    total_cost_ref = sum(seq_runs .* costs)
+    total_cost_para = sum(seq_runs[1:end-1] .* costs[1:end-1]) + div_up.(nb_of_samples[end] * parareal_intervals, ncores) * cost_para
+
+    timing[i, :] = [costs[end], cost_para, total_cost_ref, total_cost_para]
+end
+
+# mean reduction in single eval
+mean_reduction_single = mean(1 .- timing[:, 2] ./ timing[:, 1])
+mean_reduction_overall = mean(1 .- timing[:, 4] ./ timing[:, 3])
