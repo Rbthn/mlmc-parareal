d040de3ea231ffa4fb5512022d4a6014b0a72ca0
Robert Hahn <robert.hahn@tu-darmstadt.de>
2025-02-14 11:22:24 +0100

    added MLMC script for im_3kW model

---
+++ scripts/MLMC/im_3kW_extracted.jl
@@ -0,0 +1,179 @@
+# %% dependencies
+@everywhere begin
+    using DifferentialEquations
+    using Parareal
+    using SparseArrays
+    using LinearAlgebra
+    using BenchmarkTools
+end
+
+
+
+# %% Parameters
+@everywhere begin
+    const project_root = dirname(Base.active_project())
+    test_file_location = joinpath(project_root, "files/im_3kW")
+
+    # currently, there is no clever way to share these parameters between
+    # a Julia program and the GetDP .pro file. Hardcoding these values for now.
+    const freq = 50
+    const period = 1 / freq
+    const nsteps = 10
+    const dt = period / nsteps
+    const t_end = period * 8
+end
+
+
+
+# %% load system matrices
+@everywhere begin
+    include(joinpath(test_file_location, "K.jl"))    # load K
+    include(joinpath(test_file_location, "K_nu.jl")) # load K_nu
+    include(joinpath(test_file_location, "M.jl"))    # load M
+    include(joinpath(test_file_location, "rhs_coef.jl"))  # load rhs_coef
+
+    const ndof = size(K)[2]
+
+    function rhs_ansatz(t)
+        [sin(2 * pi * freq * t), sin(2 * pi * freq * t - 2 * pi / 3)]
+    end
+    function rhs_fct(t)
+        if t == 0
+            return zeros(ndof)
+        end
+        rhs_coef * rhs_ansatz(t)
+    end
+end
+
+
+
+# %% prepare Problem
+
+alg = ImplicitEuler()
+
+ode_args = (;
+    dt=dt,
+    adaptive=false,
+    saveat=1e-3
+)
+
+p = FE_Problem(
+    zeros(ndof),
+    0.0,
+    t_end,
+    dt;
+    alg=alg,
+    M=M,
+    K=K,
+    dK=K_nu,
+    r=rhs_fct,
+    ode_args...
+)
+
+
+
+# %% Parareal
+parareal_args = (;
+    parareal_intervals=8,
+    maxit=3,
+    reltol=1e-2,
+    coarse_args=(; dt=dt),
+    fine_args=(;),
+    shared_memory=false,
+)
+
+
+
+# %% MLMC
+L = 2                               # use refinement levels 0, ..., L
+mlmc_tol = 1e-1                     # desired tolerance on RMSE
+warmup_samples = 10                 # number of samples initially evaluated
+benchmark_time = 1000
+run_args = (;
+    continuate=false,
+    do_mse_splitting=true,
+    min_splitting=0.01,
+    warmup_samples=warmup_samples
+)
+
+deviations = 0.05 * [1]
+dists = Uniform.(-deviations, deviations)
+
+@everywhere function qoi_fn(sol)
+    integrate(sol.t, [abs(e[8]) for e in sol.u], SimpsonEven())
+end
+
+
+
+# %% cost model
+cost_benchmark_time = 300       # cost benchmark length
+# determine cost of single eval per level
+costs = fill(Inf, L + 1)
+for l = 0:L
+    costs[l+1] = @belapsed begin
+        n_params = length($dists)
+        params = transform.($dists, rand(n_params))
+        prob = instantiate_problem($p, params)
+        sol = DifferentialEquations.solve(
+            prob, $p.alg;
+            dt=compute_timestep($p, $l)
+        )
+        qoi = qoi_fn(sol)
+    end seconds = cost_benchmark_time
+end
+
+
+
+# %% MLMC without Parareal
+e_ref = MLMC_Experiment(p, qoi_fn, dists,
+    L, mlmc_tol;
+    use_parareal=false,
+    cost_model=(l -> costs[l[1]+1]),
+)
+@time res_ref = run(
+    e_ref;
+    run_args...,
+)
+
+bench_ref = @benchmark run(
+    e_ref;
+    run_args...,
+    verbose=false,
+) seconds = benchmark_time
+
+
+
+# %% MLMC with Parareal
+e_para = MLMC_Experiment(p, qoi_fn, dists,
+    L, mlmc_tol;
+    seed=e_ref.seed,
+    use_parareal=true,
+    parareal_args=parareal_args,
+    cost_model=(l -> costs[l[1]+1]),
+)
+@time res_para = run(
+    e_para;
+    run_args...,
+)
+
+bench_para = @benchmark run(
+    e_para;
+    run_args...,
+    verbose=false,
+) seconds = benchmark_time
+
+
+
+# %% save settings, results
+settings = (;
+    parareal_args, benchmark_time,
+    L, mlmc_tol, deviations, warmup_samples, seed=e_ref.seed, run_args
+)
+results = (;
+    costs,
+    res_ref, bench_ref,
+    res_para, bench_para
+)
+
+name = savename(p.name, settings, "jld2")
+tagsave(datadir("simulations", name), struct2dict((; settings, results)))
